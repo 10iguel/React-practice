e12e90c2d688d517bac88da96279637d813448ff
Miguel <miguel.espinoza.silloca@gmail.com>
2021-11-09 09:13:02 -0500

    More interactive updates

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './SeasonDisplay.css'
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
@@ -27,12 +28,12 @@ const SeasonDisplay = ({lat}) => {
     const {text , iconName} = seasonConfig[season]
 
     return (
-        <div>
-            <i className={`massive ${iconName} icon`}> </i>
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}> </i>
             <h1>
                 {text}
             </h1>
-            <i className={`massive ${iconName} icon`}> </i>
+            <i className={`icon-right massive ${iconName} icon`}> </i>
         </div>
     )
 
