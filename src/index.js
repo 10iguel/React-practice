ba4a1a0ac59e4faaa6ee91952028502edf9778d9
Miguel <miguel.espinoza.silloca@gmail.com>
2021-11-09 09:19:37 -0500

    Adding a spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 
 class App extends React.Component {
@@ -28,7 +29,7 @@ class App extends React.Component {
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({lat: position.coords.latitude}),
-                positionError => this.setState({errorMessage: positionError.message})
+            positionError => this.setState({errorMessage: positionError.message})
         )
     }
 
@@ -40,7 +41,7 @@ class App extends React.Component {
         if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <div> Loading...</div>
+        return <Spinner/>
 
     }
 }
